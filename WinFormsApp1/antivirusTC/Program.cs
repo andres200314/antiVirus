30162ca2b1031c8c362372beb106bae33f1c5d3b
Andres <andres111420@gmail.com>
2025-02-07 19:44:02 -0500

    backend check

--- WinFormsApp1/antivirusTC/Program.cs
+++ WinFormsApp1/antivirusTC/Program.cs
@@ -15,47 +15,35 @@ namespace antivirusTC
             
             // Analizador
             Analizador analizador = new Analizador();
-            
-            
+            List<Virus> virusEncontrados = analizador.buscarVirus(File.ReadAllBytes("..\\..\\..\\files\\completo.bin"), listaVirus);
 
+            Console.WriteLine("Lista de virus encontrados");
+            foreach (var virus in virusEncontrados)
+            {
+                Console.WriteLine(virus.getNombreVirus());
+            }
+            
         }
 
         public static List<Virus> llenarListaVirus(List<Virus> listaVirus)
         {
             StreamReader sr = new StreamReader("..\\..\\..\\files\\firmas.txt");
-            string line = sr.ReadLine();
+            string linea = sr.ReadLine();
             
-            while (line != null)
+            while (linea != null)
             {
                 listaVirus.Add(new Virus(
-                    line.Split(':')[0],  // Nombre del virus
-                    line.Split(':')[1].Split(';')  // Firma
+                    linea.Split(':')[0],  // Nombre del virus
+                    linea.Split(':')[1].Split(';')  // Firma
                         .Select(byte.Parse)  // Convertir cada valor a byte
                         .ToArray()  // Convertir a byte[]
                 ));
-                line = sr.ReadLine();
+                linea = sr.ReadLine();
             }
 
             return listaVirus;
         }
 
-        public static void crearArchivosPrueba()
-        {
-            // Definir firmas en bytes
-            byte[] firmaInicio = { 15, 30, 15, 49 };  // Firma de "Usama"
-            byte[] firmaFinal = { 30, 25, 20, 19 };   // Firma de "Covid19"
-
-            // Crear datos aleatorios
-            byte[] datosAleatorios = new byte[100]; // 100 bytes de relleno
-            new Random().NextBytes(datosAleatorios); // Llenar con valores aleatorios
-
-            // Archivo con la firma al inicio
-            File.WriteAllBytes("inicio.bin", firmaInicio.Concat(datosAleatorios).ToArray());
-            Console.WriteLine("Archivo 'inicio.bin' creado con la firma al inicio.");
-
-            // Archivo con la firma al final
-            File.WriteAllBytes("final.bin", datosAleatorios.Concat(firmaFinal).ToArray());
-            Console.WriteLine("Archivo 'final.bin' creado con la firma al final.");
-        }
+        
     }
 }
